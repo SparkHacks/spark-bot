4d65d21a96158210d8b4d6763b286efe569540ab
Kaito Sekiya <kaitosekiya@outlook.com>
2026-08-24 23:17:36 -0500

    SparkHacks Bot: small bug fixes and improvements

--- cogs/events/reaction.py
+++ cogs/events/reaction.py
@@ -16,18 +16,21 @@ class ReactionEvents(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        if str(payload.emoji) != "✅" or not payload.guild_id:
+        if payload.user_id == self.bot.user.id:
+            return
+
+        if str(payload.emoji) != "✅":
             return
 
         guild = self.bot.get_guild(payload.guild_id)
-        if get_guild_type(guild.name) != GuildType.HACKATHON:
+        if not guild or get_guild_type(guild.name) != GuildType.HACKATHON:
             return
 
         channel = guild.get_channel(payload.channel_id)
         if not channel or channel.name != hackathon.channels.RULES.name:
             return
 
-        member = guild.get_member(payload.user_id)
+        member = payload.member or guild.get_member(payload.user_id)
         if not member or member.bot:
             return
 
@@ -39,20 +42,25 @@ class ReactionEvents(commands.Cog):
         }:
             return
 
-        await member.add_roles(
-            discord.utils.get(guild.roles, name=hackathon.roles.HACKER.name)
-        )
+        hacker_role = discord.utils.get(guild.roles, name=hackathon.roles.HACKER.name)
+        if not hacker_role:
+            return
+
+        await member.add_roles(hacker_role)
         logger.info(
             f"{member} accepted rules and received Hacker role in {guild.name} server"
         )
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
-        if str(payload.emoji) != "✅" or not payload.guild_id:
+        if payload.user_id == self.bot.user.id:
+            return
+
+        if str(payload.emoji) != "✅":
             return
 
         guild = self.bot.get_guild(payload.guild_id)
-        if get_guild_type(guild.name) != GuildType.HACKATHON:
+        if not guild or get_guild_type(guild.name) != GuildType.HACKATHON:
             return
 
         channel = guild.get_channel(payload.channel_id)
